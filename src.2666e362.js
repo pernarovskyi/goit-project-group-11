c6459fd69a25bf53ce2f3888269798400d17639b
pernarovskyi <53046885+pernarovskyi@users.noreply.github.com>
2021-10-09 14:42:40 +0000

    Deploying to gh-pages from @ pernarovskyi/goit-project-group-11@df9e5551830423fce824b953c87f6273ea17ff8e 🚀

--- src.2666e362.js
+++ src.2666e362.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/dots.png":[["dots.021d9d79.png","rQHO"],"rQHO"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-project-group-11/src.037ec3af.js.map
+//# sourceMappingURL=/goit-project-group-11/src.2666e362.js.map
